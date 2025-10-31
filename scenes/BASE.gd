c6db32f6f104c60b913da388f22dfd03cbb435e1
melon <jermaise@gmail.com>
2025-10-30 21:45:37 -0600

    Added features
    There is now a feature for after a certain amount of lines inputted, it actually remembers how many children are made, and deletes the oldest ones according to a set limit(mlr)

--- scenes/BASE.gd
+++ scenes/BASE.gd
@@ -1,5 +1,6 @@
 extends Control
 const InputResponse = preload('res://scenes/dlabels.tscn')
+@export var mlr := int(30)
 @onready var rows = $Back/MarginContainer/Lines/Textinfo/ScrollContainer/HBoxContainer
 @onready var scroll = $Back/MarginContainer/Lines/Textinfo/ScrollContainer
 @onready var scrolls = scroll.get_v_scroll_bar()
@@ -13,8 +14,15 @@ func handle_scroll_changed():
 func _process(delta: float) -> void:
 	pass
 
-
+#code for ont ext entered into the box
 func _on_line_edit_text_submitted(new_text: String) -> void:
+	if new_text.is_empty():
+		return
 	var isponse = InputResponse.instantiate()
 	isponse.set_text(new_text, ' wow')
 	rows.add_child(isponse)
+	
+	if rows.get_child_count() > mlr:
+		var forgor = rows.get_child_count() - mlr
+		for lines in range(forgor):
+			rows.get_child(lines).queue_free()
