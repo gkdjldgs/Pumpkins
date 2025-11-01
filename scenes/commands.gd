6be9f2ab94625b655b43ddc14092174c12d064b4
melon <jermaise@gmail.com>
2025-11-01 00:46:45 -0600

    Sleeping for the night
    The path seems to be good, however it looks like I
    ll have to dial the expectations down for this week, I don't want to over shoot for this.
    Anyways some problems are occuring, as one of the responses is glitched out prioritizing A over everything else regardless of stage

--- scenes/commands.gd
+++ scenes/commands.gd
@@ -16,6 +16,10 @@ func command(input: String):
 				return ment(first_word)
 			'b':
 				return ment(first_word)
+			
+			'c':
+				return ment(first_word)
+				
 			'help':
 				return help()
 			_:
@@ -35,30 +39,39 @@ func ment(first_word: String):
 	elif first_word == 'a' and stage == 1:
 		stage = 2
 		return 'Unexpectedly you roll on over and start using the street instead of the sidewalk
-		As you roll forward you start to look around the halloween area wondering what next?
+		As you roll forward you start to look around this spooky night, wondering what to do next?
 		
-		A) Get hit by a car
-		B) The car sends you flying
-		C) The breeze feels great'
+		A) You are on the street
+		B) You get hit by a car
+		C) You get sent flying'
 		
-	elif stage == 3:
-		if first_word == 'a' or first_word == 'b' or first_word == 'c':
-			stage = 4
-			return 'Hold on, these options seem to be wrong...
-			Anyways the impact sends you flying into the sky like Team Rocket
-			You fly through the air but realize, GRAVITY oh NOO
-			Looking below you ponder your (real)options and choose to aim for:
-			
-			A) An unwilling particpants random house
-			B) Stinky Garbage Dump (The stench reaches to the heavens)
-			C) Who needs gravity?'
+	elif first_word == 'a' or first_word == 'b' or first_word == 'c' and stage == 2:
+		stage = 3
+		return 'Hold on, these options seem to be wrong...
+		Anyways the impact hurls you through the sky like a rocket
+		The view is honestly pretty good
+		but you realize, GRAVITY oh NOO :(
+		Looking below you ponder your (real) options and choose to:
 		
-	elif first_word == 'a' and stage == 4:
-		stage = 5
+		A) Crash an unwilling particpants random house
+		B) Cushion your body in a Stinky Garbage Dump
+		   (The stench reaches to the heavens)
+		C) Who needs gravity?'
+		
+	elif first_word == 'a' and stage == 3:
+		stage = 4
 		return "As gravity takes hold you choose to drift towards the perfectly, fine looking pastel grey house, with a cozy and clean looking design that you can't help but notice..
 		ESPECIALLY SINCE YOUR BARRELING INTO IT'S DELICATELY CRAFTED SHINGLES.
 		WOOD SPLINTERS AND AMAZINGLY YOU BOUNCE OFF A COUCH AND LAND ON THE FLOOR, UNHARMED.
-		You feel a bit shaken, and your leaves sink a bit in relief as you take a moment to relax "
+		You feel a bit shaken, and your leaves sink a bit in relief as you take a moment to relax
+		cut short by the abrupt opening of the bedroom door, 
+		followed by the entrance of the angriest man you have seen in your life.
+		YOU:
+			"
+		
+		
+
+		
 
 		
 func help() -> String:
